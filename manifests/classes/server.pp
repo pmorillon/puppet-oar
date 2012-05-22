75d269129eabdbc12115c820f535e749b0f2c4ce
Pascal Morillon <pascal.morillon@irisa.fr>
2012-05-22 11:33:26 +0200

    Implement oar::server for Debian

---
+++ manifests/classes/server.pp
@@ -0,0 +1,48 @@
+# Module:: oar
+# Manifest:: classes/server.pp
+#
+
+# Class:: oar::server ($version = "2.5") inherits oar
+#
+#
+class oar::server ($version = "2.5") {
+
+  # Allow to install frontend and server on the same machine
+  if !defined(Class["oar"]) {
+    class { "oar": version => $version; }
+  }
+
+  case $operatingsystem {
+    debian, ubuntu, centos: {
+      include "oar::server::${operatingsystem}"
+    }
+    default: {
+      err "${operatingsystem} not supported yet"
+    }
+  }
+
+} # Class:: oar::server ($version = "2.5") inherits oar
+
+
+# Class:: oar::server::debian inherits oar::server::base
+#
+#
+class oar::server::debian inherits oar::server::base {
+
+  package { "taktuk": ensure => installed; }
+
+} # Class:: oar::server::debian inherits oar::server::base
+
+# Class:: oar::server::base inherits oar
+#
+#
+class oar::server::base inherits oar {
+
+  package {
+    ["oar-server", "oar-admin"]:
+      ensure  => installed,
+      require => Oar::Configure_repo["oar"];
+  }
+
+} # Class:: oar::server::base inherits oar
+
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,40 @@
 # Module:: oar
 # Manifest:: init.pp
 #
-# Author:: Pascal Morillon (<pascal.morillon@irisa.fr>)
-# Date:: Mon May 21 14:45:03 +0200 2012
-#
 
 import "definitions/*.pp"
 import "classes/*.pp"
 
-# Class:: oar
+# Class:: oar ($version = "2.5")
 #
 #
-class oar {
-  #code...
-} # Class:: oar
+class oar ($version = "2.5") {
+
+    include "oar::dependencies"
+
+    oar::configure_repo {
+      "oar":
+        version => $version,
+        require => Package["curl"];
+    }
+
+    package {
+      ["oar-common", "oar-doc"]:
+        ensure  => installed;
+    }
+
+} # Class:: oar ($version = "2.5")
+
+
+# Class:: oar::dependencies
+#
+#
+class oar::dependencies {
+
+  package {
+    ["curl"]:
+      ensure  => installed;
+  }
+
+} # Class:: oar::dependencies
+
