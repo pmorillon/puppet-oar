dcd137ab0aad0c68606e7f8e31559348a7048815
Pascal Morillon <pascal.morillon@irisa.fr>
2012-05-23 17:38:14 +0200

    [vagrant] Allow nodes to set Alive state at boot time

--- vagrant/manifests/node.pp
+++ vagrant/manifests/node.pp
@@ -13,26 +13,30 @@ File { require => Package["oar-node"] }
 
 file {
   "${oar_home}/.ssh":
-    ensure => directory,
-    mode => 755, owner => oar, group => oar;
+    ensure  => directory,
+    mode    => 755, owner => oar, group => oar;
   "${oar_home}/.ssh/authorized_keys":
-    ensure => file,
-    mode => 644, owner => oar, group => oar,
-    source => "${files_path}/keys/node/authorized_keys";
+    ensure  => file,
+    mode    => 644, owner => oar, group => oar,
+    source  => "${files_path}/keys/node/authorized_keys";
   "${oar_home}/.ssh/id_rsa":
-    ensure => file,
-    mode => 600, owner => oar, group => oar,
-    source => "${files_path}/keys/node/oar/id_rsa";
+    ensure  => file,
+    mode    => 600, owner => oar, group => oar,
+    source  => "${files_path}/keys/node/oar/id_rsa";
   "${oar_home}/.ssh/id_rsa.pub":
-    ensure => file,
-    mode => 644, owner => oar, group => oar,
-    source => "${files_path}/keys/node/oar/id_rsa.pub";
+    ensure  => file,
+    mode    => 644, owner => oar, group => oar,
+    source  => "${files_path}/keys/node/oar/id_rsa.pub";
   "${oar_home}/.ssh/oarnodesetting_ssh.key":
-    ensure => file,
-    mode => 600, owner => oar, group => oar,
-    source => "${files_path}/keys/node/oarnodessetting/id_rsa";
+    ensure  => file,
+    mode    => 600, owner => oar, group => oar,
+    source  => "${files_path}/keys/node/oarnodessetting/id_rsa";
   "${oar_home}/.ssh/oarnodesetting_ssh.key.pub":
-    ensure => file,
-    mode => 644, owner => oar, group => oar,
-    source => "${files_path}/keys/node/oarnodessetting/id_rsa.pub";
+    ensure  => file,
+    mode    => 644, owner => oar, group => oar,
+    source  => "${files_path}/keys/node/oarnodessetting/id_rsa.pub";
+  "/etc/default/oar-node":
+    ensure  => file,
+    mode    => 644, owner => root, group => root,
+    source  =>  "${files_path}/conf/node/oar-node";
 }
