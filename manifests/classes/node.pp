67cc1c40106a727d9d3f5f81b76ca6f07a499511
Pascal Morillon <pascal.morillon@irisa.fr>
2012-05-23 16:20:05 +0200

    [oar] Implement oar::node puppet class

---
+++ manifests/classes/node.pp
@@ -0,0 +1,46 @@
+# Module:: oar
+# Manifest:: classes/node.pp
+#
+
+# Class:: oar::node ($version = "2.5")
+#
+#
+class oar::node ($version = "2.5") {
+
+  class {
+    "oar":
+      version => $version;
+  }
+
+  case $operatingsystem {
+    debian,ubuntu: {
+      include "oar::node::${operatingsystem}"
+    }
+    default: {
+      err "${operatingsystem} not supported yet"
+    }
+  }
+
+} # Class:: oar::node ($version = "2.5")
+
+# Class:: oar::node::debian inherits oar::node::base
+#
+#
+class oar::node::debian inherits oar::node::base {
+  
+
+} # Class:: oar::node::debian inherits oar::node::base
+
+# Class:: oar::node::base inherits oar
+#
+#
+class oar::node::base inherits oar {
+  
+  package {
+    "oar-node":
+      ensure  => installed,
+      require => Oar::Configure_repo["oar"];
+  }
+
+} # Class:: oar::node::base inherits oar
+
